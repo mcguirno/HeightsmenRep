832ed8edbee29b9540842eb96e9bc300603a0088
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-04-25 11:05:21 -0400

    Initial Commit

---
+++ HeightsmenRep/HeightsmenRepApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeightsmenRepApp.swift
+//  HeightsmenRep
+//
+//  Created by Noah McGuire on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct HeightsmenRepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
